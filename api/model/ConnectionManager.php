68998ac1504adbda7478f24fca9dd1b208ffcbb2
chi nguyen <lccnguyen.2022A@scis.smu.edu.com>
2023-09-24 09:00:31 +0800

    Update ConnectionManager.php

--- api/model/ConnectionManager.php
+++ api/model/ConnectionManager.php
@@ -3,15 +3,9 @@
 class ConnectionManager{
 
     public function getConnection() {
-        $servername = 'lumi-db.mysql.database.azure.com';
-        $username = 'lumiadmin';
-        $password = 'root01@bashboard';
-        $dbname = 'leaderboard';
-        $port = 3306;
-        
-        // Create connection
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);     
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // if fail, exception will be thrown
+        $con = mysqli_init();
+        mysqli_ssl_set($con,NULL,NULL, "/Users/camchinguyen/Downloads/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+        mysqli_real_connect($conn, "lumi-db.mysql.database.azure.com", "lumiadmin", "root01@dashboard", "leaderboard", 3306, MYSQLI_CLIENT_SSL);
 
         // Return connection object
         return $conn;

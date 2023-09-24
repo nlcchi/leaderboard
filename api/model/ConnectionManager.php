39660379c0c88bedecebf2f23a3a2e7650acde24
chi nguyen <lccnguyen.2022A@scis.smu.edu.com>
2023-09-24 15:31:30 +0800

    Update ConnectionManager.php

--- api/model/ConnectionManager.php
+++ api/model/ConnectionManager.php
@@ -4,7 +4,7 @@ class ConnectionManager{
 
     public function getConnection() {
         $con = mysqli_init();
-        mysqli_ssl_set($con,NULL,NULL, "/Users/camchinguyen/Downloads/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+        mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
         mysqli_real_connect($conn, "lumi-db.mysql.database.azure.com", "lumiadmin", "root01@dashboard", "leaderboard", 3306, MYSQLI_CLIENT_SSL);
 
         // Return connection object
